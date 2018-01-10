d7c845d04366e5c14e9b6aebde1554ab14b35360
YH-Chen <519617229@qq.com>
2018-01-10 13:33:31 +0800

    actionbar

--- app/src/main/java/com/example/great/project/Activities/LessonDetail.java
+++ app/src/main/java/com/example/great/project/Activities/LessonDetail.java
@@ -7,9 +7,12 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -52,16 +55,17 @@ public class LessonDetail extends AppCompatActivity {
     private TextView courseEndTime;
     private TextView courseTeacher;
     private TextView courseDate;
-    private Button changeBtn;
-    private Button deleteBtn;
-    private Button newTaskBtn;
     private RecyclerView TaskRec;
-    private TitleBar titlebar;
+
+    private TitleBar titleBar;
 
     private String sname;
     private List<Map<String, Object>> taskItem = new ArrayList<>();
     private CommonAdapter<Object> taskAdp;
     private CourseModel course;
+
+    private LayoutInflater factor;
+    private View view_in;
 
     private void initial(){
         courseRoom = findViewById(R.id.course_detail_room);
@@ -69,14 +73,10 @@ public class LessonDetail extends AppCompatActivity {
         courseEndTime = findViewById(R.id.course_detail_end_time);
         courseTeacher = findViewById(R.id.course_detail_teacher);
         courseDate = findViewById(R.id.course_detail_weekday);
-        changeBtn = findViewById(R.id.course_detail_change);
-        deleteBtn = findViewById(R.id.course_detail_delete);
-        newTaskBtn = findViewById(R.id.course_detail_new_task_btn);
         TaskRec = findViewById(R.id.course_detail_taskrec);
-        titlebar = findViewById(R.id.course_detail_titlebar);
 
         Intent intent = getIntent();
-        sname = intent.getExtras().getString("sname");
+        sname = intent.getStringExtra("sname");
         course = (CourseModel) intent.getSerializableExtra("course");
 
         courseRoom.setText(course.getRoom());
@@ -84,6 +84,13 @@ public class LessonDetail extends AppCompatActivity {
         courseEndTime.setText(course.getEndTime());
         courseTeacher.setText(course.getTeacherName());
         courseDate.setText(course.getWeekDay());
+
+        setTitle(course.getCourseName());
+        android.app.ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
 
         //任务列表初始化
         taskAdp = new CommonAdapter<Object>(this, R.layout.course_detail_task_items, generateTaskList(sname, course.getCourseId())) {
@@ -116,83 +123,6 @@ public class LessonDetail extends AppCompatActivity {
     }
 
     private void setListener(){
-        titlebar.setTitle(course.getCourseName());
-        titlebar.setLeftText("返回");
-        titlebar.setLeftImageResource(R.drawable.ic_left_black);
-        titlebar.setLeftClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setResult(2);
-                LessonDetail.this.finish();
-            }
-        });
-
-
-        changeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder editCourse = new AlertDialog.Builder(LessonDetail.this);
-                editCourse.setTitle("修改课程");
-                LayoutInflater factor = LayoutInflater.from(LessonDetail.this);
-                View view_in = factor.inflate(R.layout.course_edit_dialog_layout, null);
-                editCourse.setView(view_in);
-                final EditText editCourseName = view_in.findViewById(R.id.course_edit_name);
-                final EditText editCourseRoom = view_in.findViewById(R.id.course_edit_room);
-                final EditText editCourseStartHour = view_in.findViewById(R.id.course_edit_start_hour);
-                final EditText editCourseStratMinute = view_in.findViewById(R.id.course_edit_start_minute);
-                final EditText editCourseEndHour = view_in.findViewById(R.id.course_edit_end_hour);
-                final EditText editCourseEndMinute = view_in.findViewById(R.id.course_edit_end_minute);
-                final EditText editCourseTeacher = view_in.findViewById(R.id.course_edit_teacher);
-                final Spinner editCourseweekday = view_in.findViewById(R.id.course_edit_weekday);
-                editCourseName.setText(course.getCourseName());
-                editCourseRoom.setText(course.getRoom());
-                editCourseTeacher.setText(course.getTeacherName());
-                editCourse.setPositiveButton("修改课程", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        course.setRoom(editCourseRoom.getText().toString());
-                        course.setCourseName(editCourseName.getText().toString());
-                        course.setStartTime(editCourseStartHour.getText().toString() + ":" + editCourseStratMinute.getText().toString());
-                        course.setEndTime(editCourseEndHour.getText().toString() + ":" + editCourseEndMinute.getText().toString());
-                        course.setWeekDay(editCourseweekday.getSelectedItem().toString());
-                        course.setTeacherName(editCourseTeacher.getText().toString());
-                        if (!editCourseName.getText().toString().isEmpty()) cdb.updateCourse(sname, course);
-                        courseRoom.setText(course.getRoom());
-                        courseStratTime.setText(course.getStartTime());
-                        courseEndTime.setText(course.getEndTime());
-                        courseTeacher.setText(course.getTeacherName());
-                        courseDate.setText(course.getWeekDay());
-                    }
-                });
-                editCourse.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {}
-                });
-                editCourse.show();
-            }
-        });
-
-        deleteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder deleteCourse = new AlertDialog.Builder(LessonDetail.this);
-                deleteCourse.setTitle("确认删除该课程？");
-                deleteCourse.setPositiveButton("删除课程", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        cdb.deleteCourse(sname, course.getCourseId());
-                        setResult(2);
-                        LessonDetail.this.finish();
-                    }
-                });
-                deleteCourse.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {}
-                });
-                deleteCourse.show();
-            }
-        });
-
         taskAdp.setOnItemClickListener(new CommonAdapter.OnItemClickListener() {
             @Override
             public void onClick(int position) {
@@ -265,36 +195,6 @@ public class LessonDetail extends AppCompatActivity {
                 }
             }
         });
-        newTaskBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder addTaskAlertDialog = new AlertDialog.Builder(LessonDetail.this);
-                addTaskAlertDialog.setTitle("任务");
-                LayoutInflater factor = LayoutInflater.from(LessonDetail.this);
-                View view_in = factor.inflate(R.layout.course_detail_add_task_dialog_layout, null);
-                addTaskAlertDialog.setView(view_in);
-                final EditText editTaskName = view_in.findViewById(R.id.course_detail_add_task_dialog_taskname);
-                final EditText editTaskBrief = view_in.findViewById(R.id.course_detail_add_task_dialog_taskbrief);
-                final DatePicker editTaskDDL = view_in.findViewById(R.id.course_detail_add_task_dialog_taskDDL);
-                addTaskAlertDialog.setPositiveButton("添加", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Date d = new Date(editTaskDDL.getYear()-1900, editTaskDDL.getMonth(), editTaskDDL.getDayOfMonth());
-                        int newId = tdb.newTask(new Task(1, course.getCourseId(), editTaskName.getText().toString(),
-                                editTaskBrief.getText().toString(), d , sname), true);
-                        //加入后得到id
-                        Task temp = new Task(newId, course.getCourseId(), editTaskName.getText().toString(),
-                                editTaskBrief.getText().toString(), d , sname);
-                        taskAdp.addItem(1, temp);
-                    }
-                });
-                addTaskAlertDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {}
-                });
-                addTaskAlertDialog.show();
-            }
-        });
     }
 
     @Override
@@ -321,5 +221,105 @@ public class LessonDetail extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         initial();
         setListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.lesson_set, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.backHome:
+                finish();
+                return true;
+            case R.id.addTask:
+                Log.d("TAG", "添加任务");
+                AlertDialog.Builder addTaskAlertDialog = new AlertDialog.Builder(LessonDetail.this);
+                addTaskAlertDialog.setTitle("任务");
+                factor = LayoutInflater.from(LessonDetail.this);
+                view_in = factor.inflate(R.layout.course_detail_add_task_dialog_layout, null);
+                addTaskAlertDialog.setView(view_in);
+                final EditText editTaskName = view_in.findViewById(R.id.course_detail_add_task_dialog_taskname);
+                final EditText editTaskBrief = view_in.findViewById(R.id.course_detail_add_task_dialog_taskbrief);
+                final DatePicker editTaskDDL = view_in.findViewById(R.id.course_detail_add_task_dialog_taskDDL);
+                addTaskAlertDialog.setPositiveButton("添加", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Date d = new Date(editTaskDDL.getYear()-1900, editTaskDDL.getMonth(), editTaskDDL.getDayOfMonth());
+                        int newId = tdb.newTask(new Task(1, course.getCourseId(), editTaskName.getText().toString(),
+                                editTaskBrief.getText().toString(), d , sname), true);
+                        //加入后得到id
+                        Task temp = new Task(newId, course.getCourseId(), editTaskName.getText().toString(),
+                                editTaskBrief.getText().toString(), d , sname);
+                        taskAdp.addItem(1, temp);
+                    }
+                });
+                addTaskAlertDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {}
+                }).show();
+                return true;
+            case R.id.modify_course:
+                AlertDialog.Builder editCourse = new AlertDialog.Builder(LessonDetail.this);
+                editCourse.setTitle("修改课程");
+                factor = LayoutInflater.from(LessonDetail.this);
+                view_in = factor.inflate(R.layout.course_edit_dialog_layout, null);
+                editCourse.setView(view_in);
+                final EditText editCourseName = view_in.findViewById(R.id.course_edit_name);
+                final EditText editCourseRoom = view_in.findViewById(R.id.course_edit_room);
+                final EditText editCourseStartHour = view_in.findViewById(R.id.course_edit_start_hour);
+                final EditText editCourseStratMinute = view_in.findViewById(R.id.course_edit_start_minute);
+                final EditText editCourseEndHour = view_in.findViewById(R.id.course_edit_end_hour);
+                final EditText editCourseEndMinute = view_in.findViewById(R.id.course_edit_end_minute);
+                final EditText editCourseTeacher = view_in.findViewById(R.id.course_edit_teacher);
+                final Spinner editCourseweekday = view_in.findViewById(R.id.course_edit_weekday);
+                editCourseName.setText(course.getCourseName());
+                editCourseRoom.setText(course.getRoom());
+                editCourseTeacher.setText(course.getTeacherName());
+                editCourse.setPositiveButton("修改课程", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        course.setRoom(editCourseRoom.getText().toString());
+                        course.setCourseName(editCourseName.getText().toString());
+                        course.setStartTime(editCourseStartHour.getText().toString() + ":" + editCourseStratMinute.getText().toString());
+                        course.setEndTime(editCourseEndHour.getText().toString() + ":" + editCourseEndMinute.getText().toString());
+                        course.setWeekDay(editCourseweekday.getSelectedItem().toString());
+                        course.setTeacherName(editCourseTeacher.getText().toString());
+                        if (!editCourseName.getText().toString().isEmpty()) cdb.updateCourse(sname, course);
+                        courseRoom.setText(course.getRoom());
+                        courseStratTime.setText(course.getStartTime());
+                        courseEndTime.setText(course.getEndTime());
+                        courseTeacher.setText(course.getTeacherName());
+                        courseDate.setText(course.getWeekDay());
+                    }
+                });
+                editCourse.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {}
+                }).show();
+                return true;
+            case R.id.delete_course:
+                AlertDialog.Builder deleteCourse = new AlertDialog.Builder(LessonDetail.this);
+                deleteCourse.setTitle("确认删除该课程？");
+                deleteCourse.setPositiveButton("删除课程", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        cdb.deleteCourse(sname, course.getCourseId());
+                        setResult(2);
+                        LessonDetail.this.finish();
+                    }
+                });
+                deleteCourse.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {}
+                }).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/great/project/Activities/MainActivity.java
+++ app/src/main/java/com/example/great/project/Activities/MainActivity.java
@@ -933,6 +933,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         initial();
         setListener();
